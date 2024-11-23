9065968e54b8a4af362ede1c84419eda9d0f7901
blfmelissa <melissabouloufa@gmail.com>
2024-11-23 16:09:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,17 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, Azure!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
-
 # Connexion à la base de données
 @app.on_event("startup")
-def startup_db():
-    global connection
+async def startup_db():
     connection = pyodbc.connect(
         'DRIVER={ODBC Driver 17 for SQL Server};'
         'SERVER=tcp:mlizsqlserver.database.windows.net,1433;'
         'DATABASE=books-db;'
         'UID=melissa;'
-        'PWD={your_password}'
+        'PWD=781227moi!'
     )
+    print("Database connection etablished!")
 
 @app.on_event("shutdown")
 def shutdown_db():
